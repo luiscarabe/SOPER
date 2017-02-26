40ccb7b62eeb0e4c4df0e9b7becd1ca114371446
luiscarabe <luiscarabe@gmail.com>
2017-02-27 00:24:07 +0100

    odio git

--- practica1/ejercicio8.c
+++ practica1/ejercicio8.c
@@ -3,13 +3,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
-#define LENGHT 80
 
 int main (int argc, char** argv){
     int pid = 1;
     int i;    
     char *prog[2];
+    char *aux;
 
     if (argc < 2){
         printf("Error en los argumentos de entrada");
@@ -27,32 +28,48 @@ int main (int argc, char** argv){
         else if(pid == 0){
             prog[0] = argv[i+1];
             prog[1] = NULL;
+            /*Reservamos memoria para aux, donde guardaremos el path de los programas*/
+            aux = (char*)malloc(4*strlen(prog[0])*sizeof(char));
+
+            if(aux == NULL){
+                perror("Error al resevar memoria.\n");
+                exit(EXIT_FAILURE);
+            }
+            
+            sprintf(aux, "/bin/%s", prog[0]);
+
 
             if((strcmp(argv[argc-1], "-l")) == 0){
-                execl(prog[0], prog[0], NULL);
+                execl(aux, prog[0], NULL);
                 perror("Error en exec");
+                free(aux);
                 exit(EXIT_FAILURE);
             }
 
             else if((strcmp(argv[argc-1], "-lp")) == 0){
                 execlp(prog[0], prog[0], NULL);
                 perror("Error en exec");
+                free(aux);
                 exit(EXIT_FAILURE);
             }
             else if((strcmp(argv[argc-1], "-v")) == 0){
-                execv(prog[0], prog);
+                execv(aux, prog);
                 perror("Error en exec");
+                free(aux);
                 exit(EXIT_FAILURE);
             }
             else if((strcmp(argv[argc-1], "-vp")) == 0){
                 execvp(prog[0], prog);
                 perror("Error en exec");
+                free(aux);
                 exit(EXIT_FAILURE);
             }
             else {
                 printf("Error en el último parámetro.\n");
             } 
+            free(aux);
         }
+
     }
 
     for(i=0; i < (argc -2); i++){
