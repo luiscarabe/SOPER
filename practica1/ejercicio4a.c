8150c7592c50e079fdbabfe3629d19658f3f6758
emiliocuestaf <emiliocuestaf@gmail.com>
2017-03-03 11:44:26 +0100

    doxygen primeros ejs done

--- practica1/ejercicio4a.c
+++ practica1/ejercicio4a.c
@@ -1,3 +1,13 @@
+/**
+* @brief Programa correspondiente al ejercicio 4a de la practica 1 de sistemas operativos
+*
+* Este programa se utilizara para estudiar si se generan procesos hijo que se puedan quedar huerfanos.
+* 
+* @file ejercicio4a.c
+* @author Luis Carabe y Emilio Cuesta
+* @date 3-03-2017
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -7,18 +17,24 @@
 
 #define NUM_PROC 3
 
+/**
+ * @brief Funcion main del programa.
+ * @return EXIT_SUCCESS si no ha habido errores, EXIT_FAILURE en caso contrario.
+ */
 int main (void){
-	int pid;
-	int i;
-	for (i=0; i < NUM_PROC; i++){
-		if ((pid=fork()) < 0 ){
-			printf("Error al emplear fork\n");
-			exit(EXIT_FAILURE);
-		} else if (pid == 0){
+    int pid;
+    int i;
+
+    for (i=0; i < NUM_PROC; i++){
+        if ((pid=fork()) < 0 ){
+            printf("Error al emplear fork\n");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0){
             printf("HIJO %d con pid %d y ppid %d.\n", i, getpid(), getppid());
-		} else {
-			printf ("PADRE %d con pid %d\n", i, getpid());
-		}
-	}
-	exit(EXIT_SUCCESS);
+        } else {
+            printf ("PADRE %d con pid %d\n", i, getpid());
+        }
+    }
+
+    exit(EXIT_SUCCESS);
 }
--- practica1/ejercicio5a.c
+++ practica1/ejercicio5a.c
@@ -1,3 +1,14 @@
+/**
+* @brief Programa correspondiente al ejercicio 5a de la practica 1 de sistemas operativos
+*
+* Este programa se utilizara para generar procesos en serie, es decir, 
+* de tal forma que cada proceso tenga tan solo un hijo.
+* 
+* @file ejercicio5a.c
+* @author Luis Carabe y Emilio Cuesta
+* @date 3-03-2017
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -6,20 +17,27 @@
 
 #define NUM_PROC 3
 
+/**
+ * @brief Funcion main del programa.
+ * @return EXIT_SUCCESS si no habido errores, EXIT_FAILURE en caso contrario.
+ */
 int main (void){
-	int pid=0;
-	int i;
-	printf("PADRE principal, mi pid es el %d\n", getpid());
-	for (i=0; i < NUM_PROC && pid == 0; i++){
-		if ((pid=fork()) < 0 ){
-			printf("Error haciendo fork\n");
-			exit(EXIT_FAILURE);
-		}else if (pid == 0){
-			printf("HIJO con i: %d, mi pid es el %d y el de mi padre el %d.\n", i, getpid(), getppid());
-		}else{
-			printf ("PADRE %d\n", i);
-		}
-	}
+    int pid;
+    int i;
+
+    printf("PADRE principal, mi pid es el %d\n", getpid());
+    pid=0;
+    for (i=0; i < NUM_PROC && pid == 0; i++){
+        if ((pid=fork()) < 0 ){
+            printf("Error haciendo fork\n");
+            exit(EXIT_FAILURE);
+        }else if (pid == 0){
+            printf("HIJO con i: %d, mi pid es el %d y el de mi padre el %d.\n", i, getpid(), getppid());
+        }else{
+            printf ("PADRE %d\n", i);
+        }
+    }
+
     wait(NULL);
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
